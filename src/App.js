ffaf62805b420e22e391239cc24594c8c9cb797d
addem <bedouiadem8@gmail.com>
2022-12-04 17:41:58 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import DisplayTodos from "./todo/DisplayTodos";
+import Todos from "./todo/Todos";
+
+
+function App() {
+  return (
+    <div>
+      <Todos/>
+      <DisplayTodos/>
+    </div>
+  );
+}
+
+export default App;
